e1e93b85b56b364e6102aba70d1e03cf776fe1bd
lebrancconvas <lebrancconvas@gmail.com>
2022-04-15 13:51:31 +0700

    ✨ High-Level Function.

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,19 @@
-import * as cheerio from 'cheerio';
-import axios from 'axios';
-import fs from 'fs';
+import {getText, writeText} from './lib/scrape';
 
+const baselink: string = "https://en.wikipedia.org/wiki/";
+const topic: string = "Square Enix"; 
 
-const getHTML = async(url: string) => {
-	const {data} = await axios.get(url);
-	return data;
-}
+const pathtopic = topic.replace(' ', '_');
+const name = topic.replace(' ', '');
 
-const writeHTML = async(url: string, filename: string) => {
-	fs.writeFile(`./data/output/html/${filename}.html`, await getHTML(url), (err) => {
-		if(err) {
-			console.error(err);
-			return;
-		}
-	});
-};
+const link: string = `${baselink}${pathtopic}`;
 
-const getText = async(url: string) => {
-	const $ = cheerio.load(await getHTML(url));
-	console.log($.text());
-}
-
-getText("https://bootcamp.uxdesign.cc/website-footer-best-practices-what-avoid-ed7a2481d3af"); 
-
+writeText(link, name)
+.then(data => {
+	if(data !== undefined) {
+		console.log(data);
+	} else {
+		console.log("Success Working !!"); 
+	}
+})
+.catch(err => console.error(err)); 
