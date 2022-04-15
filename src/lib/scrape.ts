f65d58bfe56b1f86f18cd2e3822fb3c1b6fed518
lebrancconvas <lebrancconvas@gmail.com>
2022-04-15 13:51:17 +0700

    🚚 Seperate Scraping Function.

---
+++ src/lib/scrape.ts
@@ -0,0 +1,34 @@
+import * as cheerio from 'cheerio';
+import axios from 'axios';
+import fs from 'fs';
+
+
+export const getHTML = async(url: string) => {
+	const {data} = await axios.get(url);
+	return data;
+}
+
+export const writeHTML = async(url: string, filename: string) => {
+	fs.writeFile(`./data/output/html/${filename}.html`, await getHTML(url), (err) => {
+		if(err) {
+			console.error(err);
+			return;
+		}
+	});
+};
+
+export const getText = async(url: string) => {
+	const $ = cheerio.load(await getHTML(url));
+	return $.text();
+}
+
+export const writeText = async(url: string, filename: string) => {
+	fs.writeFile(`./data/output/text/${filename}.txt`, await getText(url), (err) => {
+		if(err) {
+			console.error(err);
+			return;
+		}
+	});
+}
+
+
